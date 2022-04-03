b1f181f222186f26fca7ed689a699d7379b338b0
Carlos <carlos.sigma66@gmail.com>
2022-04-02 22:37:37 -0600

    Comentarios en el script de python para dejar documentado lo que hace cada bloque de codigo

--- scripts/sociedades_to_json.py
+++ scripts/sociedades_to_json.py
@@ -1,24 +1,25 @@
 import sqlite3
 import json
-
 from numpy import append
 
-# Conexion
+# Conexion a la base de datos
 conn = sqlite3.connect("C:/Users/cmoreno/OneDrive/Cursos Practicos/Proyecto Python/Catalogos/catalogos.db")
 
-# Crear cursor
+# Crear cursor para gestionar los queries de sql
 cursor = conn.cursor()
-
-lista  = []
-dicc = {}
-keys = ['soc', 'socedad', 'pais', 'moneda']
 cursor.execute(f"SELECT * FROM sociedades")
 resultado = cursor.fetchall()
 
+# Se crea la lista vacia para poner los diccionarios
+lista = []
+
+# Se itera sobre el resultado del query de sql
 for item in resultado:
+    # se crea cada diccionario para hacer la lista de diccionarios para convertir a json
     dict_item = {'soc':item[0], 'sociedad':item[1], 'pais':item[2], 'moneda':item[3]}
     lista.append(dict_item)
 
+# Se graba la lista de duccionarios en formato json
 with open('db1.jason', 'w') as f:
     json.dump(lista,f)
 
